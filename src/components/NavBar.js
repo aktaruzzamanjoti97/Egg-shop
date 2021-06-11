edaa0e1edba81737efe330c328e7274716012f9d
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-06-11 09:15:28 +0600

    everything done

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const NavBar = ({toggle}) => {
+    return (
+        <nav className="flex justify-between items-center h-16 bg-gray-300 text-black relative text-xl shadow-sm font-mono" role="navigation">
+            <Link to='/' className="pl-8 w-16 md:w-26 lg:w-18"><img src="https://images.squarespace-cdn.com/content/570fa9a8d51cd494834bfe9a/1472732390019-NXKOH4I4M159R5SBZ4KC/Virgo_website+favicons_egg.png?content-type=image%2Fpng" alt="" /></Link>
+            <div className="px-4 cursor-pointer md:hidden"
+                onClick={toggle}
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+            </div>
+            <div className="pr-8 md:block hidden">
+                <Link className="p-4" to="/">Home</Link>
+                <Link className="p-4" to="/menu">Menu</Link>
+                <Link className="p-4" to="/about">About</Link>
+                <Link className="p-4" to="/contact">Contact</Link>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
